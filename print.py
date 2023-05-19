b70612b86f037fe66f73ca900c71ecfeacac9322
abreur3beca <123771701+abreur3beca@users.noreply.github.com>
2023-05-19 11:49:34 -0300

    primeira aula de python

---
+++ print.py
@@ -0,0 +1,16 @@
+print("mensagem que eu quero", end=". ")
+
+print("fulano \n é meu \"amigo\"")
+
+print("1 x 1 =",1*1)
+
+print("10² =",10**3)
+
+nome = ""
+print(nome)
+nome = 1
+print(nome)
+nome = 1+1
+print(nome)
+nome = True
+print(nome)
